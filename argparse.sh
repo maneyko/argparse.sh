8231bc3e7ab96c123acf5ab2dde471cbccbb50a8
Peter Maneykowski <pmaneyko@gmail.com>
2020-04-23 09:37:46 -0500

    add performance improvements, try not to call awk

--- argparse.sh
+++ argparse.sh
@@ -18,19 +18,33 @@ OPTIONAL_SINGLE_DESCRIPTIONS=()
 
 HELP_DESCRIPTION=
 
+parse_arg1() {
+  t1="${1%%\]*}"
+  echo "${t1#\[}"
+}
+
+parse_arg2() {
+  t1="${1#*[ ]}"
+  parse_arg1 "$t1"
+}
+
+parse_arg3() {
+  t1="${1##*\[}"
+  echo "${t1%\]}"
+}
+
 arg_positional_single() {
-  IFS="|" read arg_name arg_desc <<< "$(
-    echo "$1" | awk -F '[][]' '{print $2 "|" $4}'
-  )"
+  arg_name="$(parse_arg1 "$1")"
+  arg_desc="$(parse_arg3 "$1")"
   POSITIONAL_NAMES+=($arg_name)
   POSITIONAL_DESCRIPTIONS+=("$arg_desc")
   parse_args $arg_name "positional"
 }
 
 arg_optional_single() {
-  IFS="|" read arg_name arg_flag arg_desc <<< "$(
-    echo "$1" | awk -F '[][]' '{print $2 "|" $4 "|" $6}'
-  )"
+  arg_name="$(parse_arg1 "$1")"
+  arg_flag="$(parse_arg2 "$1")"
+  arg_desc="$(parse_arg3 "$1")"
   OPTIONAL_SINGLE_NAMES+=($arg_name)
   OPTIONAL_SINGLE_FLAGS+=($arg_flag)
   OPTIONAL_SINGLE_DESCRIPTIONS+=("$arg_desc")
@@ -38,9 +52,9 @@ arg_optional_single() {
 }
 
 arg_optional_boolean() {
-  IFS="|" read arg_name arg_flag arg_desc <<< "$(
-    echo "$1" | awk -F '[][]' '{print $2 "|" $4 "|" $6}'
-  )"
+  arg_name="$(parse_arg1 "$1")"
+  arg_flag="$(parse_arg2 "$1")"
+  arg_desc="$(parse_arg3 "$1")"
   OPTIONAL_BOOLEAN_NAMES+=($arg_name)
   OPTIONAL_BOOLEAN_FLAGS+=($arg_flag)
   OPTIONAL_BOOLEAN_DESCRIPTIONS+=("$arg_desc")
@@ -50,7 +64,7 @@ arg_optional_boolean() {
 arg_help() {
   arg_name="help"
   arg_flag="h"
-  HELP_DESCRIPTION="$(echo "$1" | awk -F '[][]' '{print $2}')"
+  HELP_DESCRIPTION="$(parse_arg3 "$1")"
   OPTIONAL_BOOLEAN_NAMES+=($arg_name)
   OPTIONAL_BOOLEAN_FLAGS+=($arg_flag)
   OPTIONAL_BOOLEAN_DESCRIPTIONS+=("Print this help message.")
@@ -69,29 +83,27 @@ print_help() {
   i=0
   for opt_name in "${OPTIONAL_SINGLE_NAMES[@]}"; do
     opt_flag="${OPTIONAL_SINGLE_FLAGS[@]}"
-    printf "[-$opt_flag $opt_name]"
+    printf "[-$opt_flag $opt_name] "
   done
-  echo; echo
-  echo "$HELP_DESCRIPTION"
-  echo
+  echo -e "\n\n$HELP_DESCRIPTION\n"
   if test -n "${POSITIONAL_NAMES}"; then
     echo 'positional arguments:'
     i=0
     for p_name in "${POSITIONAL_NAMES[@]}"; do
-      echo -e "  $p_name\t${POSITIONAL_DESCRIPTIONS[$i]}"
+      printf "  %-25s ${POSITIONAL_DESCRIPTIONS[$i]}\n" "$p_name"
       i=$(($i + 1))
     done
     if test -n "${OPTIONAL_SINGLE_NAMES}" -o -n "${OPTIONAL_BOOLEAN_NAMES}"; then
-      echo -e '\noptional arguments:'
+      echo -e "\noptional arguments:"
     fi
     i=0
     for bool_name in "${OPTIONAL_BOOLEAN_NAMES[@]}"; do
-      echo -e "  -${OPTIONAL_BOOLEAN_FLAGS[$i]}, --$bool_name\t${OPTIONAL_BOOLEAN_DESCRIPTIONS[$i]}"
+      printf "  %-25s ${OPTIONAL_BOOLEAN_DESCRIPTIONS[$i]}\n" "-${OPTIONAL_BOOLEAN_FLAGS[$i]}, --$bool_name"
       i=$(($i + 1))
     done
     i=0
     for opt_name in "${OPTIONAL_SINGLE_NAMES[@]}"; do
-      echo -e "  -${OPTIONAL_SINGLE_FLAGS[$i]}, --$opt_name\t${OPTIONAL_SINGLE_DESCRIPTIONS[$i]}"
+      printf "  %-25s ${OPTIONAL_SINGLE_DESCRIPTIONS[$i]}\n" "-${OPTIONAL_SINGLE_FLAGS[$i]}, --$opt_name"
       i=$(($i + 1))
     done
   fi
@@ -100,18 +112,21 @@ print_help() {
 
 parse_args() {
   name_upper="$(echo $1 | tr '/a-z/' '/A-Z/' | tr '-' '_')"
+
   if test "$2" = "positional"; then
     arg_val="$(echo "$ARGS_STR" | perl -ne '/[^-][^-]?([\S]+)/ && print $1')"
     eval "$(printf "ARG_%s=%s" "$name_upper" "$arg_val")"
+
   elif test -n "$(echo "$ARGS_STR" | awk "/--$1/ || /-$2/")"; then
-    if test -z "$3"; then
+    if test -n "$3"; then
+      eval "$(printf "ARG_%s=$3" "$name_upper")"
+    else
       arg_val="$(echo "$ARGS_STR" \
         | perl -ne "( /--$1 ([\S]+)/ || /-$2[ ]?([\S]+)/ ) && print \$1")"
       eval "$(printf "ARG_%s=%s" "$name_upper" "$arg_val")"
-    else
-      eval "$(printf "ARG_%s=true" "$name_upper")"
     fi
   fi
+
   if test -n "$ARG_HELP"; then
     print_help
     exit 0
