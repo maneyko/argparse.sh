d2abc7cc578bdfaaf509663fcc02f67e7822e8c9
Peter Maneykowski <pmaneyko@gmail.com>
2020-12-05 15:38:26 -0600

    modify usage example

--- usage-example.sh
+++ usage-example.sh
@@ -2,12 +2,12 @@
 
 source "$(dirname "$0")/argparse.sh"
 
-arg_positional "[infile] [The file to parse]"
-arg_positional "[outfile] [The output file]"
+arg_positional "[infile]          [The file to parse]"
+arg_positional "[outfile]         [The output file]"
 arg_optional   "[port-number] [p] [The port number]"
-arg_optional   "[outputs] [o] [The number of outputs]"
-arg_boolean    "[verbose] [v] [Do verbose output]"
-arg_boolean    "[flag] [f] [My important flag]"
+arg_optional   "[outputs]     [o] [The number of outputs]"
+arg_boolean    "[verbose]     [v] [Do verbose output]"
+arg_boolean    "[flag]        [f] [My important flag]"
 
 read -d '' helptxt << EOT
 This file illustrates how argparse.sh can be used
@@ -17,10 +17,11 @@ EOT
 arg_help              "[$helptxt]"
 parse_args
 
-
-echo "infile:      $ARG_INFILE"
-echo "outfile:     $ARG_OUTFILE"
-echo "port-number: $ARG_PORT_NUMBER"
-echo "outputs:     $ARG_OUTPUTS"
-echo "verbose:     $ARG_VERBOSE"
-echo "flag:        $ARG_FLAG"
+cat << EOT
+infile:      $ARG_INFILE
+outfile:     $ARG_OUTFILE
+port-number: $ARG_PORT_NUMBER
+outputs:     $ARG_OUTPUTS
+verbose:     $ARG_VERBOSE
+flag:        $ARG_FLAG
+EOT
